2e12fb2de6507447971694b08e690ffa7aeb879b
AndreyVjs <andreyvjs@gmail.com>
2026-04-28 09:31:32 -0300

    Add files via upload

---
+++ Para-Lanches/js/script.js
@@ -0,0 +1,23 @@
+
+function verificarlogin(){
+
+    var login = document.getElementById("login").value;
+    var senha = document.getElementById("senha").value;
+
+    if(login == "123" && senha == "admin"){
+        alert("Login correto! Bem-vindo ao Parah Lanches");
+    }else{
+        alert("Login ou senha incorretos");
+    }
+
+}
+
+function mostrarSenha(){
+    var campo = document.getElementById("senha");
+
+    if(campo.type === "password"){
+        campo.type = "text";
+    }else{
+        campo.type = "password";
+    }
+}
