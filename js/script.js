c76fcdf9b4a21262a978fc99b97240eae8195ad7
AndreyVjs <andreyvjs@gmail.com>
2026-06-28 22:19:19 -0300

    Refactor login verification and password display functions

--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
 
-function verificarlogin(){
+// funcao que verifica se o login e senha estao corretos
+function verificarlogin() {
 
+    // pegando o que o usuario digitou nos campos
     var login = document.getElementById("login").value;
     var senha = document.getElementById("senha").value;
 
-    if(login == "123" && senha == "admin"){
+    // se o login e senha baterem, libera o acesso
+    if (login == "login" && senha == "senha") {
         alert("Login correto! Bem-vindo ao Parah Lanches");
-    }else{
+    } else {
+        // se nao bater, avisa que ta errado
         alert("Login ou senha incorretos");
     }
 
 }
 
-function mostrarSenha(){
+// funcao que mostra ou esconde a senha no campo
+function mostrarSenha() {
     var campo = document.getElementById("senha");
 
-    if(campo.type === "password"){
+    // se tiver como password, muda pra text pra mostrar
+    if (campo.type === "password") {
         campo.type = "text";
-    }else{
+    } else {
+        // se ja tiver mostrando, esconde de novo
         campo.type = "password";
     }
 }
